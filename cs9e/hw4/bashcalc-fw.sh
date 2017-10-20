bffa4bc569e7a53983d4d90dfed797446fe75bcf
clzls119 <hsong1101@gmail.com>
2017-10-20 00:05:01 -0700

    cs9e hw4

--- cs9e/hw4/bashcalc-fw.sh
+++ cs9e/hw4/bashcalc-fw.sh
@@ -5,45 +5,35 @@
 ## Floating Point Math Functions ##
 
 # bashcalc <expression>
-# This function simply passes in the given expressions to 'bc -l' and prints the result
 
 function bashcalc {
 	echo $1 | bc -l	# ADD CODE HERE FOR PART 2
 }
 
-# Remove this line when you start part 3
-return 0
-
-# sine <expression>
-# This function prints the cosine of the given expression
 function sine {
-# ADD CODE HERE FOR PART 3
+	echo "s($1)" | bc -l
 }
 
-# cosine <expression>
-# This function prints the cosine of the given expression
 function cosine {
-	# ADD CODE HERE FOR PART 3
+	echo "c($1)" | bc -l	# ADD CODE HERE FOR PART 3
 }
 
-# angle_reduce <angle>
-# Prints the angle given expressed as a value between 0 and 2pi
 function angle_reduce {
-	# ADD CODE HERE FOR PART 3
+	rem=$(echo "$1 % 1" | bc)
+	result=$(echo "8*a($rem)" | bc -l)
+	echo $result
 }
 
-# float_{lt,lte,eq} <expr 1> <expr 2>
-# These functions returns true (exit code 0) if the first value is less than the second (lt),
-# less than or equal to the second (lte), or equal to the second (eq).
-# Note: We can't just use BASH's builtin [[ ... < ... ]] operator because that is
-#       for integer math.
 function float_lt {
-	# ADD CODE HERE FOR PART 3
+	result=$(echo "$1 < $2" | bc -l)
+	return $result
 }
 function float_eq {
-# ADD CODE HERE FOR PART 3
+	result=$(echo "$1==$2" | bc -l)
+	return $result
 }
 
 function float_lte {
-#ADD CODE HERE FOR PART 3
+	result=$(echo "$1<=$2" | bc -l)
+	return $result
 }
