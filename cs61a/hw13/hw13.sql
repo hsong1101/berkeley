0c38228d4cfe187887f68514f1a32c0db6fe4e52
Hanmaro Song <hsong1101@gmail.com>
2017-11-27 12:55:44 -0800

    update

--- cs61a/hw13/hw13.sql
+++ cs61a/hw13/hw13.sql
@@ -13,6 +13,11 @@ CREATE TABLE flights AS
   SELECT "LAS"             , "SLC"           , 50          UNION
   SELECT "LAX"             , "PDX"           , 89               ;
 
+CREATE TABLE stores AS
+  SELECT "Hallmart" AS store, "50 Lawton Way" AS address, 25 AS Mbs UNION
+  SELECT "Targive"          , "2 Red Circle Way"        , 40        UNION
+  SELECT "RestBuy"          , "1 Kiosk Ave"             , 30             ;
+
 CREATE TABLE main_course AS
   SELECT "turkey" AS meat, "cranberries" AS side, 2000 AS calories UNION
   SELECT "turducken"     , "potatoes"           , 4000             UNION
@@ -26,22 +31,14 @@ CREATE TABLE main_course AS
   SELECT "chicken"       , "cranberries"        , 2500             UNION
   SELECT "turducken"     , "butter"             , 10000            UNION
   SELECT "turducken"     , "more_butter"        , 15000                 ;
-
+  
 CREATE TABLE pies AS
   SELECT "pumpkin" AS pie, 500 AS calories UNION
   SELECT "apple"         , 400             UNION
   SELECT "chocolate"     , 600             UNION
   SELECT "cherry"        , 550                  ;
 
-CREATE TABLE products AS
-  SELECT "phone" AS category, "uPhone" AS name, 99.99 AS MSRP, 4.5 AS rating UNION
-  SELECT "phone"            , "rPhone"        , 79.99        , 3             UNION
-  SELECT "phone"            , "qPhone"        , 89.99        , 4             UNION
-  SELECT "games"            , "GameStation"   , 299.99       , 3             UNION
-  SELECT "games"            , "QBox"          , 399.99       , 3.5           UNION
-  SELECT "computer"         , "iBook"         , 112.99       , 4             UNION
-  SELECT "computer"         , "wBook"         , 114.29       , 4.4           UNION
-  SELECT "computer"         , "kBook"         , 99.99        , 3.8                ;
+
 
 CREATE TABLE inventory AS
   SELECT "Hallmart" AS store, "uPhone" AS item, 99.99 AS price UNION
@@ -75,43 +72,74 @@ CREATE TABLE inventory AS
   SELECT "Hallmart"         , "kBook"         , 95.99          UNION
   SELECT "Targive"          , "kBook"         , 96.99          UNION
   SELECT "RestBuy"          , "kBook"         , 94.99               ;
-
-CREATE TABLE stores AS
-  SELECT "Hallmart" AS store, "50 Lawton Way" AS address, 25 AS Mbs UNION
-  SELECT "Targive"          , "2 Red Circle Way"        , 40        UNION
-  SELECT "RestBuy"          , "1 Kiosk Ave"             , 30             ;
-
+  
+CREATE TABLE products AS
+  SELECT "phone" AS category, "uPhone" AS name, 99.99 AS MSRP, 4.5 AS rating UNION
+  SELECT "phone"            , "rPhone"        , 79.99        , 3             UNION
+  SELECT "phone"            , "qPhone"        , 89.99        , 4             UNION
+  SELECT "games"            , "GameStation"   , 299.99       , 3             UNION
+  SELECT "games"            , "QBox"          , 399.99       , 3.5           UNION
+  SELECT "computer"         , "iBook"         , 112.99       , 4             UNION
+  SELECT "computer"         , "wBook"         , 114.29       , 4.4           UNION
+  SELECT "computer"         , "kBook"         , 99.99        , 3.8                ;
 
 -------------------------------------------------------------
 -- PLEASE DO NOT CHANGE ANY SQL STATEMENTS ABOVE THIS LINE --
 -------------------------------------------------------------
 
-CREATE TABLE schedule AS
+CREATE TABLE lowest_prices AS
+  SELECT store, item, min(price) from inventory group by item;
+  
+CREATE TABLE shopping_list AS
   with
-    transfer(n, d, a, p) as (
-        select 1, departure, arrival, price from flights union
-        select n+1, d, a, p from transfer where n < 3 order by p
+    best_item(n, price) as (
+      select name, min(MSRP / rating) from products group by category
     )
-  select d || ', ' || a from transfer where d = "SFO" or a = "PDX";
+  SELECT i.n, store from best_item as i, inventory where n = item;
   
 
-CREATE TABLE number_of_options AS
-  SELECT 4;
-
-CREATE TABLE calories AS
-  SELECT 22;
+CREATE TABLE average_prices AS
+  select category, avg(MSRP) from products group by category;
+  
 
 CREATE TABLE healthiest_meats AS
-  SELECT "tofurky|1400" union select "turkey|1900";
+  with
+    healthy(meat_option, side_option, pie_option, m_cal, total_cal) as (
+      select m.meat, m.side, p.pie, m.calories, m.calories+p.calories from main_course as m, pies as p union
+      select meat_option, side_option, pie_option, m_cal, total_cal from healthy order by total_cal
+    )
+  SELECT distinct meat_option, min(total_cal) from healthy where total_cal < 2900 group by meat_option;
 
-CREATE TABLE average_prices AS
-  SELECT "computer|109.09" union select "games|349.99" union select "phone|89.99";
 
-CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE number_of_options AS
+    select count(distinct meat) from main_course;
+    
 
-CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE calories AS
+  with
+    option(meat_option, side_option, pie_option, cal) as (
+        select m.meat, m.side, p.pie, m.calories + p.calories from main_course as m, pies as p union
+        select meat_option, side_option, pie_option, cal from option
+    )
+  SELECT count(*) from option where cal < 2500;
+  
+
+CREATE TABLE schedule AS
+  with
+    transfer(d, a, n, p) as (
+        select departure, arrival, 1, price from flights where departure = "SFO" union
+        select d || ', ' || a, arrival, n+1, p+price from transfer, flights where n < 3 and a != arrival
+    )
+  select distinct d || ', ' || a, p from transfer where n < 3 and a = "PDX" order by p;
+
+
+
+
+
+
+
+
+
 
 CREATE TABLE total_bandwidth AS
   SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
